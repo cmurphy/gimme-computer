ccf79d6d765f6c59335a4e87e276fd6de167c8d8
Colleen Murphy <colleen.murphy@suse.com>
2017-08-04 15:07:07 +0200

    Set password for devuser

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -6,6 +6,7 @@ export PATH=$PATH:$DDD_DIR/scripts
 export IMAGE_DIR=${IMAGE_DIR:-${HOME}/.gimme-images}
 export DIB_DEV_USER_PWDLESS_SUDO=${DIB_DEV_USER_PWDLESS_SUDO:-Yes}
 export DIB_DEV_USER_SHELL=/bin/bash
+export DIB_DEV_USER_PASSWORD=${DIB_DEV_USER_PASSWORD:-devuser}
 export IMAGE_NAME=${IMAGE_NAME:-}
 export LIBVIRT_DEFAULT_URI=qemu:///system
 export VM_MEMORY=${VM_MEMORY:-4096}
